a11e535fccaf4f680aa3bf7155ea1d5e020886cf
Dzenzinetta <alhpa.dante0@gmail.com>
2024-02-22 11:44:26 +0200

    Modified files below: 	modified:   lazy-lock.json 	modified:   lua/dzenzinetta/core/mapping.lua 	modified:   lua/dzenzinetta/core/plugins-mapping.lua Add new mappings

--- lua/dzenzinetta/core/mapping.lua
+++ lua/dzenzinetta/core/mapping.lua
@@ -7,3 +7,12 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+vim.keymap.set('n', '<C-h>', '<C-w>h', {silent = true})
+vim.keymap.set('n', '<C-l>', '<C-w>l', {silent = true})
+vim.keymap.set('n', '<C-j>', '<C-w>j', {silent = true})
+vim.keymap.set('n', '<C-k>', '<C-w>k', {silent = true})
+
+vim.keymap.set('n', '<TAB>', '<Cmd>BufferLineCycleNext<CR>', {silent = true})
+vim.keymap.set('n', '<S-TAB>', '<Cmd>BufferLineCyclePrev<CR>', {silent = true})
+vim.keymap.set('n', '<F3>', '<Cmd>NvimTreeFindFileToggle<CR>', { silent = true })
